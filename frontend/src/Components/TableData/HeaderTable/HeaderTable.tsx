f1695ca617035828edb7479414f29da2b6879d76
Артем Чмиль <artem.chmil@bk.ru>
2023-07-27 21:01:36 +0300

    done sort

--- frontend/src/Components/TableData/HeaderTable/HeaderTable.tsx
+++ frontend/src/Components/TableData/HeaderTable/HeaderTable.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import "./HeaderTable.css";
-function HeaderTable() {
+function HeaderTable({
+  onSortId,
+  onSortTitle,
+  onSortBody,
+}: {
+  onSortId: () => void;
+  onSortTitle: () => void;
+  onSortBody: () => void;
+}) {
   return (
     <div className="headerTable">
       <div className="idHeader" style={{ color: "white" }}>
         <span>ID</span>
-        <button>Sort Id</button>
+        <button onClick={onSortId}>Sort Id</button>
       </div>
       <div className="nameCaptionHeader">
         <span style={{ color: "white" }}>Заголовок</span>
-        <button>sort</button>
+        <button onClick={onSortTitle}>sort title</button>
       </div>
       <div className="nameDescriptionHeader">
         <span style={{ color: "white" }}>Описание</span>
-        <button>sort</button>
+        <button onClick={onSortBody}>sort body</button>
       </div>
     </div>
   );
--- frontend/src/Components/TableData/Table/Table.tsx
+++ frontend/src/Components/TableData/Table/Table.tsx
@@ -6,9 +6,11 @@ import { initialListData } from "../dataSlice";
 import TableOneColumn from "./TableOneColumn";
 import "./Css/Table.css";
 import SelectPage from "./SelectPage";
+import { sortDataListById } from "../dataSlice";
 
 function Table() {
   const [page, setPage] = useState(1);
+  const [sortOrder, setSortOrder] = useState("asc");
   const { dataList } = useSelector((store: RootState) => store.data);
   const dispatch = useAppDispatch();
 
@@ -16,6 +18,49 @@ function Table() {
     setPage(pageNumber);
   };
 
+  const sortDataById = () => {
+    if (sortOrder === "asc") {
+      const sortedList = [...dataList].sort((a, b) => a.id - b.id);
+      dispatch(sortDataListById(sortedList));
+      setSortOrder("desc");
+    } else {
+      const sortedList = [...dataList].sort((a, b) => b.id - a.id);
+      dispatch(sortDataListById(sortedList));
+      setSortOrder("asc");
+    }
+  };
+
+  const sortDataByTittle = () => {
+    if (sortOrder === "asc") {
+      const sortedList = [...dataList].sort((a, b) =>
+        a.title.localeCompare(b.title)
+      );
+      dispatch(sortDataListById(sortedList));
+      setSortOrder("desc");
+    } else {
+      const sortedList = [...dataList].sort((a, b) =>
+        b.title.localeCompare(a.title)
+      );
+      dispatch(sortDataListById(sortedList));
+      setSortOrder("asc");
+    }
+  };
+
+  const sortDataByBody = () => {
+    if (sortOrder === "asc") {
+      const sortedList = [...dataList].sort((a, b) =>
+        a.body.localeCompare(b.body)
+      );
+      dispatch(sortDataListById(sortedList));
+      setSortOrder("desc");
+    } else {
+      const sortedList = [...dataList].sort((a, b) =>
+        b.body.localeCompare(a.body)
+      );
+      dispatch(sortDataListById(sortedList));
+      setSortOrder("asc");
+    }
+  };
   const startIndex = (page - 1) * 10;
   const endIndex = page * 10;
 
@@ -26,7 +71,11 @@ function Table() {
   console.log(dataList);
   return (
     <div>
-      <HeaderTable />
+      <HeaderTable
+        onSortId={sortDataById}
+        onSortTitle={sortDataByTittle}
+        onSortBody={sortDataByBody}
+      />
       <div className="data">
         {dataList.slice(startIndex, endIndex).map((data) => (
           <TableOneColumn data={data} key={data.id} />
