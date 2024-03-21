375da946942be326d34797831e5ac067cf31fac4
Artem Chmil <artem.chmil@bk.ru>
2024-03-21 13:32:50 +0300

    Delete src directory

--- src/Components/TableData/api.ts
+++
@@ -1,6 +0,0 @@
-import { Data } from "./Types/dataTypes";
-
-export const initDataList = async (): Promise<Data[]> => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  return res.json();
-};
