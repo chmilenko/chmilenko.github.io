477208166010357e17793f70c551b33b8cc620ce
Shirota <artem.chmil@bk.ru>
2024-03-21 14:15:56 +0300

    add arrow

--- src/Components/TableData/HeaderTable/HeaderTable.tsx
+++ src/Components/TableData/HeaderTable/HeaderTable.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./HeaderTable.css";
-// import img from "../../../../public/img/strelka";
+const img = require("./arrow.png") as string;
 
 function HeaderTable({
   onSortId,
@@ -15,15 +15,15 @@ function HeaderTable({
     <div className="headerTable">
       <div className="idHeader" style={{ color: "white", fontWeight: 600 }}>
         <span>ID</span>
-        <img className="arrow" onClick={onSortId} alt="sort" />
+        <img className="arrow" onClick={onSortId} alt="sort" src={img} />
       </div>
       <div className="nameCaptionHeader">
         <span style={{ color: "white", fontWeight: 600 }}>Заголовок</span>
-        <img className="arrow" onClick={onSortTitle} alt="sort" />
+        <img className="arrow" onClick={onSortTitle} alt="sort" src={img} />
       </div>
       <div className="nameDescriptionHeader">
         <span style={{ color: "white", fontWeight: 600 }}>Описание</span>
-        <img className="arrow" onClick={onSortTitle} alt="sort" />
+        <img className="arrow" onClick={onSortTitle} alt="sort" src={img} />
       </div>
     </div>
   );
